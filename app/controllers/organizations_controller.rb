3b9d5b95d0777aea0f8e0b003eb3aadb0a6a095a
Samo <samo.zeleznik5@gmail.com>
2018-02-03 12:43:11 +0100

    update action

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,11 +2,12 @@ class OrganizationsController < ApplicationController
   def update
     organization = Organization.friendly.find(params[:id])
     organization.update(organization_params)
+    organization.set_updated_at
   end
 
   private
 
   def organization_params
-
+    params.required(:organization).permit(:payload)
   end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,12 +1,11 @@
 class Organization
   include Mongoid::Document
   include Mongoid::Slug
-
+  include Mongoid::Timestamps
+  
   field :name
   slug :name, history: true
   field :url
   field :payload
   field :documentation
-  field :created_at, type: DateTime
-  field :updated_at, type: DateTime
 end
