a153aadc0f7157f5edd7b7568c94f5e216646166
Samo <samo.zeleznik5@gmail.com>
2018-02-03 13:51:20 +0100

    remove controller, add worker

---
+++ app/workers/api_worker.rb
@@ -0,0 +1,18 @@
+class ApiWorker
+  include Sidekiq::Worker
+  require 'nokogiri'
+  require 'httparty'
+
+  def perform(id)
+    organization = Organization.find(id)
+    url = organization.url
+    response = HTTParty.get(url, :verify => false).parsed_response
+    if response.class == Hash
+      json_payload = response
+    else
+      doc = Nokogiri::HTML(response)
+      json_payload = JSON.parse(doc.at('body').content)
+    end
+    organization.update(payload: json_payload)
+  end
+end
