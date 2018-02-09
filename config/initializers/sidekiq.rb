6669ff5ea098ed1438d59a921e0f8264b2604b68
Luka Domitrovič <luka@domitrovic.si>
2018-02-09 12:33:05 +0100

    setup sidekiq and mongo for docker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,8 @@
 require "redis"
-redis_conn = proc {
-  Redis.new # do anything you want here
-}
+
 Sidekiq.configure_client do |config|
-  config.redis = ConnectionPool.new(size: 5, &redis_conn)
+  config.redis = { url: 'redis://redis:6379/0' }
 end
 Sidekiq.configure_server do |config|
-  config.redis = ConnectionPool.new(size: 25, &redis_conn)
+  config.redis = { url: 'redis://redis:6379/0' }
 end
