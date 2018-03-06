17b6bbdc1c9496d00010e7969e1cf9fbde053a55
Samo <samo.zeleznik5@gmail.com>
2018-03-06 18:36:09 +0100

    helper algorithm

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,2 +1,21 @@
 module OrganizationsHelper
+  def create_form_from_json(payload)
+    payload.map do |key, value|
+      if value.is_a?(Hash)
+         if create_form_from_json(value)
+           create_fields(key,value)
+         end
+      elsif value.is_a?(Array)
+        create_form_from_json(value.first)
+      else
+        return true
+      end
+    end
+  end
+
+  private
+
+  def create_fields(key,value)
+    "#{key}: " + "Type: #{value[:type]}" + "Description: #{value[:description]}"
+  end
 end
