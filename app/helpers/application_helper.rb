79540df2f418bdc4d87405425eecb454a86b72ae
Samo <samo.zeleznik5@gmail.com>
2018-03-07 13:27:29 +0100

    create form, add links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def display_session_links
+    if organization_signed_in?
+      link_to 'Sign out', destroy_organization_session_path, method: :delete
+    else
+      link_to 'Sign in', new_organization_session_path
+    end
+  end
 end
--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -1,21 +1,41 @@
 module OrganizationsHelper
-  def create_form_from_json(payload)
-    payload.map do |key, value|
-      if value.is_a?(Hash)
-         if create_form_from_json(value)
-           create_fields(key,value)
-         end
-      elsif value.is_a?(Array)
-        create_form_from_json(value.first)
-      else
-        return true
-      end
+  def create_form_from_payload(payload,f)
+    hash = crate_hash_from_payload(payload)
+    html=""
+    hash.map do |key, value|
+      html += '<div>'
+      html += f.label key.to_sym
+      html += label_tag value.keys.first.to_sym
+      html += select_tag value.keys.first.to_sym, options_for_select(DataType.all.collect{ |t| [t.name, t.id] })
+      html += label_tag value.keys.last.to_sym
+      html += text_field_tag value.keys.last.to_sym
+      html += '</div>'
     end
+    html.html_safe
   end
 
   private
 
-  def create_fields(key,value)
-    "#{key}: " + "Type: #{value[:type]}" + "Description: #{value[:description]}"
+  def crate_hash_from_payload(payload)
+    hash = Hash.new
+    payload.map do |key, value|
+      if value.is_a?(Hash)
+         if hash = hash.merge(crate_hash_from_payload(value))
+           hash[key] = create_fields(value) unless value[:type].nil? && value[:description].nil?
+         end
+      elsif value.is_a?(Array)
+        hash = hash.merge(crate_hash_from_payload(value.first))
+      else
+        return hash
+      end
+    end
+    return hash
+  end
+
+  def create_fields(value)
+    {
+      type: value[:type],
+      description: value[:description]
+    }
   end
 end
