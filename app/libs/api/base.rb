331d0ce20f9ed29d5d8b3b2d479dd285eb78929f
Luka Domitrovič <luka@domitrovic.si>
2018-03-19 08:07:52 +0100

    add base api lib

---
+++ app/libs/api/base.rb
@@ -0,0 +1,48 @@
+module Api
+  class Base
+    attr_reader :conn
+
+    def api_get(path, params = {})
+      Rails.logger.info "GET #{path}, params: #{params.to_json}"
+      resp = conn.get(path, params)
+      handle_resp(resp)
+    end
+
+    def api_post(path, data = {})
+      Rails.logger.info "POST #{path}, data: #{data.to_json}"
+      resp = conn.post(path) do |req|
+        req.body = data.to_json
+      end
+      handle_resp(resp)
+    end
+
+    def api_put(path, data = {})
+      Rails.logger.info "PUT #{path}, data: #{data.to_json}"
+      resp = conn.put(path) do |req|
+        req.body = data.to_json
+      end
+      handle_resp(resp)
+    end
+
+    def api_delete(path, params = {})
+      Rails.logger.info "DELETE #{path}, params: #{params.to_json}"
+      resp = conn.delete(path, params)
+      handle_resp(resp)
+    end
+
+    def handle_resp(resp)
+      case resp.status
+      when 200..299
+        resp.body.presence ? JSON.parse!(resp.body) : nil
+      when 404
+        raise Api::NotFoundError.new(resp), "Resource not found: #{resp.body}"
+      when 400..499
+        raise Api::ClientError.new(resp), "Server returned #{resp.status}: #{resp.body}"
+      when 500..599
+        raise Api::ServerError.new(resp), "Server returned #{resp.status}: #{resp.body}"
+      else
+        raise Api::Error.new(resp), "Server returned #{resp.status}: #{resp.body}"
+      end
+    end
+  end
+end
