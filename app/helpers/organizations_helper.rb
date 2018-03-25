d47c6668a7506aafde3365f404687936a301f392
Samo <samo.zeleznik5@gmail.com>
2018-03-25 18:36:42 +0200

    set api form edit

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -3,19 +3,27 @@ module OrganizationsHelper
 
   def create_form_from_payload(payload,f)
     hash = crate_hash_from_payload(payload)
-    html=""
+    html = '<div class="box-body">'
     hash.map do |key, value|
-      html += '<div>'
-      html += f.label key
-      html += f.label value.keys.first
-      html += select_tag value.keys.first.to_s+"[#{key}]", options_for_select(DataType.all.collect{ |t| [t.name, t.id] })
-      html += f.label value.keys.last
-      html += text_field_tag value.keys.last.to_s+"[#{key}]", '', required: true
-      html += '</div>'
+      html += <<-HTML
+        <div class="form-group">
+          <h2>#{key}</h2>
+          #{f.label 'Podatkovni tip'}
+          #{select_tag value.keys.first.to_s+"[#{key}]", options_for_select(DataType.all.collect{ |t| [t.name, t.id] }), { :class => 'form-control' }}
+        </div>
+        <div class="form-group">
+          #{f.label 'Opis'}
+          #{text_field_tag value.keys.last.to_s+"[#{key}]", '', required: true, placeholder: 'Opis', class: 'form-control'}
+        </div>
+        <hr />
+      HTML
     end
-    html += '<div>'
-    html += f.submit 'Potrdi'
-    html += '</div>'
+    html += <<-HTML
+      </div>
+      <div class="box-footer">
+        #{f.submit 'Potrdi', class: 'btn btn-primary'}
+      </div>
+    HTML
     html.html_safe
   end
 end
