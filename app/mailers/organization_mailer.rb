d77d1b5df893ef1a6bf9acabf460f8c33617334f
Samo <samo.zeleznik5@gmail.com>
2018-05-11 15:27:22 +0200

    fix mailer subject

--- app/mailers/organization_mailer.rb
+++ app/mailers/organization_mailer.rb
@@ -1,11 +1,11 @@
 class OrganizationMailer < ApplicationMailer
   def account_approved(organization)
     @organization = organization
-    mail(to: @organization.email, subject: 'Your organization has been confirmed')
+    mail(to: @organization.email, subject: 'Vaša organizacija je bila potrjena.')
   end
 
   def new_organization(organization)
     @organization = organization
-    mail(to: 'open-data@velenje.si', subject: 'New organization has requested access.')
+    mail(to: 'open-data@velenje.si', subject: 'Nova organizacija zahteva dostop.')
   end
 end
